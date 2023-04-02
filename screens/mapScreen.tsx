6fb7cd47e4278739921b9ea65b3b459260b76a5a
Michael Simire <simiremichael@gmail.com>
2023-04-03 00:18:23 +0100

    updated

--- screens/mapScreen.tsx
+++ screens/mapScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+//import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { Marker } from 'react-native-maps';
+import Mapbox from '@rnmapbox/maps';
+import { MapView } from '@rnmapbox/maps';
 
 export default function MapScreen({route}: any) {
+
+  Mapbox.setAccessToken('pk.eyJ1Ijoic2ltaXJlbWljaGFlbCIsImEiOiJjbDhtMWZza3owOGM5M290aGdkdXNzbnhyIn0.cZ53EbJgw_QlQEq2-bRpWw');
 
   const {longitude, latitude} = route.params;
   console.log(longitude, latitude);
@@ -11,20 +15,20 @@ export default function MapScreen({route}: any) {
 
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}
-      // coordinate={{ latitude : latitude , longitude : longitude }} 
-      initialRegion={{
-        latitude: (Number(latitude)),
-        longitude: (Number(longitude)),
-        latitudeDelta: 0.0422,
-        longitudeDelta: 0.0121,
-      }}
+      <Mapbox.MapView style={styles.map}
+      //coordinate={{ latitude : latitude , longitude : longitude }} 
+      // initialRegion={{
+      //   latitude: (Number(latitude)),
+      //   longitude: (Number(longitude)),
+      //   latitudeDelta: 0.0422,
+      //   longitudeDelta: 0.0121,
+      // }}
       >
      <Marker coordinate={{
           latitude: (Number(latitude)),
           longitude: (Number(longitude))
         }} />
-      </MapView>
+      </Mapbox.MapView>
     </View>
   );
 }
